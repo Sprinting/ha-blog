dab21158a33f56d52d03ae454f338fe467e5a40c
Kartik <kartik1807@gmail.com>
2024-09-12 02:32:55 +0200

    vv simple edsl, case..of, types, newtypes, pattern-matching

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,55 @@
-module Main where
+-- hello.hs
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn (render myhtml)
+
+myhtml :: Html
+myhtml =
+  html_
+    "My title"
+    ( append_
+      (h1_ "Heading")
+      ( append_
+        (p_ "Paragraph #1")
+        (p_ "Paragraph #2")
+      )
+    )
+
+newtype Html = 
+  Html String
+newtype HtmlElement = 
+  HtmlElement String
+type Title = 
+  String
+
+append_ :: HtmlElement -> HtmlElement -> HtmlElement
+append_ (HtmlElement a) (HtmlElement b) =
+  HtmlElement (a <> b)
+
+render :: Html -> String
+render (Html h) = 
+  h
+
+getHtmlElementString :: HtmlElement -> String
+getHtmlElementString content =
+  case content of
+    HtmlElement str -> str
+
+html_ :: Title -> HtmlElement -> Html
+html_ title content = 
+  Html
+    ( el "html"
+      (  el "head" (el "title" title )
+            <> el "body" (getHtmlElementString content)
+      )
+    )
+
+p_ :: String -> HtmlElement
+p_ = HtmlElement . el "p"
+
+h1_ :: String -> HtmlElement
+h1_ = HtmlElement . el "h1"
+
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
